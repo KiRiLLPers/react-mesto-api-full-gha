fc1f3a897aeaad17ce65ac041c3ab3c915794233
Kirill <dorc99@mail.ru>
2023-08-28 14:41:36 +0700

    случайно удалил длину соли

--- backend/consts/consts.js
+++ backend/consts/consts.js
@@ -1,5 +1,6 @@
-const JWT_SECRET = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'my-secret-key';
-
+const JWT_SECRET = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'my-secreet';
+const SALT_ROUND = 10;
 module.exports = {
+  SALT_ROUND,
   JWT_SECRET,
 };
